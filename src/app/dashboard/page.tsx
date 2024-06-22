eddfdccdeb29538844e34c03207ef2d02afdf85f
root <biuro.norsk@gmail.com>
2024-06-22 08:06:22 +0200

    added auth by clerk

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>This is a protected dashboard page</p>
+    </div>
+  );
+}
---
+++ src/app/logout/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Logout () {
+  return (
+    <div>
+      <h1>You have been logged out</h1>
+      <p>Thank you for using F1Whispers</p>
+    </div>
+  );
+}
