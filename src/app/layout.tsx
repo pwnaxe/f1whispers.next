e6ec950462b9e3943f10afbf08920cbb45d1f261
root <biuro.norsk@gmail.com>
2024-06-23 08:23:36 +0200

    added clerk navbar post

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,47 @@
-import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
-import './globals.css';
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import Script from "next/script";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://clerk-next-app.vercel.app/"),
+  title: "Next.js Clerk Template",
+  description:
+    "A simple and powerful Next.js template featuring authentication and user management powered by Clerk.",
+  openGraph: { images: ["/og.png"] },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body>
-          <header>
-            <SignedOut>
-              <SignInButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
-          <main>
-            {children}
-          </main>
+    <html lang="en">
+      <ClerkProvider
+        appearance={{
+          variables: { colorPrimary: "#000000" },
+          elements: {
+            formButtonPrimary:
+              "bg-black border border-black border-solid hover:bg-white hover:text-black",
+            socialButtonsBlockButton:
+              "bg-white border-gray-200 hover:bg-transparent hover:border-black text-gray-600 hover:text-black",
+            socialButtonsBlockButtonText: "font-semibold",
+            formButtonReset:
+              "bg-white border border-solid border-gray-200 hover:bg-transparent hover:border-black text-gray-500 hover:text-black",
+            membersPageInviteButton:
+              "bg-black border border-black border-solid hover:bg-white hover:text-black",
+            card: "bg-[#fafafa]",
+          },
+        }}
+      >
+        <body className={`min-h-screen flex flex-col antialiased`}>
+          {children}
         </body>
-      </html>
-    </ClerkProvider>
-  )
+      </ClerkProvider>
+
+      <Script src="https://cdn.jsdelivr.net/npm/prismjs@1/components/prism-core.min.js" />
+      <Script src="https://cdn.jsdelivr.net/npm/prismjs@1/plugins/autoloader/prism-autoloader.min.js" />
+    </html>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import link from 'next/link';
+import Navbar from './components/Navbar';
+import Post from './components/Post';
+import Privacy from './components/Privacy';
+
 
 function App() {
   return (
     <>
+      <Navbar />
       <h1>App</h1>
+      <Post />
+      {/* <Privacy /> */}
     </>
   );
 }
